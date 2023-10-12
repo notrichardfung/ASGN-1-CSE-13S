5a14cd77fc156a4416b241c34168f4ad60b11ee6
notrichardfung <97049049+notrichardfung@users.noreply.github.com>
2023-10-12 16:33:31 -0700

    Update log_analyzer.sh

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
+# Check if the number of arguments is 2
+if [ "$#" -ne 2 ]; then
     echo "Invalid number of arguments"
     exit 1
 fi
-
-log_file=$1
-# Calculate the total content size and the count of unique response codes
-
-total_size=0
-declare -A response_codes
-while read -r line; do
-    # Parse the content size and response code from the log line
-    content_size=$(echo "$line" | awk '{print $NF}')
-    response_code=$(echo "$line" | awk '{print $(NF-1)}')
-    # Treat "-" as 0 and add the content size to the total
-    if [ "$content_size" = "-" ]; then
-    content_size=0
+# Read the stop words from the file and store them in an array
+stop_words=($(<stopwords))
+# Read the text file, convert all the words to lowercase, and remove the stop words
+# Count the frequency of each word and store the results in an associative array
+declare -A word_count
+while read -r word; do
+    word=${word,,}
+    if [[ ! " ${stop_words[@]} " =~ " ${word} " ]]; then
+        ((word_count[$word]++))
     fi
-    ((total_size += content_size))
-    # Increment the count for the current response code
-    ((response_codes[$response_code]++))
-done < "$log_file"
-
-# Calculate the average content size and round to the nearest integer
-num_requests=$(wc -l < "$log_file")
-average_size=$(( (total_size + (num_requests/2)) / num_requests ))
-
-# Count the number of unique response codes
-num_response_codes=${#response_codes[@]}
-
-echo "$average_size"
-echo "$num_response_codes"
+done < "$1"
+# Print the top k most frequent words
+for word in $(printf '%s\n' "${!word_count[@]}" | sort -rnk2 | head -n "$2"); do
+    echo "$word"
+done
+exit 0
